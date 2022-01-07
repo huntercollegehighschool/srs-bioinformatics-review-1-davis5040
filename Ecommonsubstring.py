ecb15bb5169fd7b9a62e0905ef4c90fc122c5f2a
Davis Zong <93000346+davis5040@users.noreply.github.com>
2022-01-07 20:11:05 +0000

    Solved E

--- Ecommonsubstring.py
+++ Ecommonsubstring.py
@@ -10,36 +10,49 @@ commonsubstring("ACGCT", "CGCCA", "ATTACGCT") should return ["CGC"]
 """
 from Antcheck import isDNA
 
+def issubstring(a, b): #is a a substring of b?
+  la = len(a)
+  lb = len(b)
+  if la > lb: return "OOPS"
+  else:
+    ans = False
+    cur = -1
+    for i in range(lb - la + 1):
+      cur += 1
+      if b[cur:cur+la] == a: ans = True
+    return ans
+
+
 def commonsubstring(dnalist):
-  s = ""
-  sz = 1000000000
-  for x in dnalist:
-    if isDNA(x) == False: return "error"
-    if len(x) <= sz:
+  s = "" #shortest dna string
+  sz = 1000000000 #size of shortest dna
+  for x in dnalist: 
+    if isDNA(x) == False: 
+      return "error" #check if DNA is valid
+    if len(x) <= sz: #find shortest dna
       s = x
       sz = len(x)
-  best = -1
-  d = []
-  l = sz
-  for i in range(sz-1):
-    cur = 0
+  best = -1 #length of best common dna
+  d = [] #list of answers with max length
+  l = sz+1 #size of current string
+  for i in range(sz):
+    l -= 1 #increment length
+    cur = -1#size of interval
     for j in range(sz-l+1):
+      cur += 1#increment interval
       ch = s[cur:cur+l]
-      pos = False
-      for k in dnalist:
-        good = False
-        cur1 = 0
-        it = len(k) - l + 1
-        for m in range(it):
-          if k[cur1:cur1+l] == ch:
-            good = True
-        if good == True: pos = True
-        cur1 = cur1 + 1
-      if pos == True: 
-        if len(ch) >= best:
+      flag = False
+      for st in dnalist:
+        if issubstring(ch, st) == False: 
+          flag = True
+      if flag == False: 
+        d.append(ch)
+        if best < len(ch):
           best = len(ch)
-          d.append(ch)
-    cur = cur + 1
-  return d
+  lis = []
+  for y in d:
+    if len(y) == best:
+      if y not in lis: lis.append(y)
+  return lis
 
 
--- main.py
+++ main.py
@@ -9,11 +9,11 @@ part = (input("Which part(A, B, C, D, E)?")).upper()
 while part not in ['A', 'B', 'C', 'D', 'E']:
   os.system('clear')
   print("That's not a valid part.")
-  part = int(input("Which part(A, B, C, D, E)?")).upper()
+  part = input("Which part(A, B, C, D, E)?").upper()
 
 os.system('clear')
 
-dna = ["ACTGAAAGTG", "CTGAAAGT", "GGAAAGCC" "TTAACCGAAAG", "ACD"]
+dna = ["AAT", "AAT", "AAT", "GTA"]
 
 if part == "A":
   from Antcheck import isDNA
@@ -37,4 +37,4 @@ elif part == "D":
 
 elif part == "E":
   from Ecommonsubstring import commonsubstring
-  print("Common substring:", commonsubstring(dna[:-1]))
+  print("Common substring:", commonsubstring(dna))
